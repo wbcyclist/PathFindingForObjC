fe2b73f89f32fbcb2332bbe21568f006eecda1da
Blazej SLEBODA <contact@blazejsleboda.fr>
2016-11-17 13:35:27 +0100

    Revert "Fix : Multiple methods named 'state' found with mismatched result, parameter type or attributes"
    This reverts commit 9b3cae657402c88cba43c7eb217c10a3b6921e0b.

--- PathFindingForObjC-Example/OSX/StackCellViewController.m
+++ PathFindingForObjC-Example/OSX/StackCellViewController.m
@@ -9,7 +9,6 @@
 
 #import "StackCellViewController.h"
 
-#import <Quartz/Quartz.h>
 
 #define DISCLOSED_HEIGHT 320
 
@@ -362,8 +361,8 @@
 		NSButtonCell *selCell = [sender selectedCell];
 		self.movementType = (int)selCell.tag;
 	} else if ([sender tag]==2) {
-		self.isBidirectional = [sender state];
-        //self.isBidirectional = [((NSButton *)sender) state];
+		//self.isBidirectional = [sender state];
+        self.isBidirectional = [((NSButton *)sender) state];
 	}
 }
 

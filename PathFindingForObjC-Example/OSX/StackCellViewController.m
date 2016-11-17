b09765a270e2b56c7633c6ae855994fefe023b7e
Blazej SLEBODA <contact@blazejsleboda.fr>
2016-11-17 13:45:22 +0100

    Fix : Multiple methods named 'state' found with mismatched result, parameter type or attributes

--- PathFindingForObjC-Example/OSX/StackCellViewController.m
+++ PathFindingForObjC-Example/OSX/StackCellViewController.m
@@ -362,7 +362,7 @@
 		NSButtonCell *selCell = [sender selectedCell];
 		self.movementType = (int)selCell.tag;
 	} else if ([sender tag]==2) {
-		self.isBidirectional = [sender state];
+		self.isBidirectional = [((NSButton *)sender) state];
 	}
 }
 

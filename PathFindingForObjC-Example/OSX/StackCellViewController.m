9b3cae657402c88cba43c7eb217c10a3b6921e0b
Blazej SLEBODA <contact@blazejsleboda.fr>
2016-11-17 13:32:06 +0100

    Fix : Multiple methods named 'state' found with mismatched result, parameter type or attributes Fix : Definition of 'CAMediaTimingFunction' must be imported from module 'QuartzCore.CAMediaTimingFunction' before it is required

--- PathFindingForObjC-Example/OSX/StackCellViewController.m
+++ PathFindingForObjC-Example/OSX/StackCellViewController.m
@@ -9,6 +9,7 @@
 
 #import "StackCellViewController.h"
 
+#import <Quartz/Quartz.h>
 
 #define DISCLOSED_HEIGHT 320
 
@@ -361,8 +362,8 @@
 		NSButtonCell *selCell = [sender selectedCell];
 		self.movementType = (int)selCell.tag;
 	} else if ([sender tag]==2) {
-		//self.isBidirectional = [sender state];
-        self.isBidirectional = [((NSButton *)sender) state];
+		self.isBidirectional = [sender state];
+        //self.isBidirectional = [((NSButton *)sender) state];
 	}
 }
 
